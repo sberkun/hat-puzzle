bdb499e0a1384afa53e2ffd4df7c8011ded85362
rsambouncer <rsambouncer@gmail.com>
2017-11-08 11:17:36 -0800

    Update player.js

--- backend/player.js
+++ backend/player.js
@@ -1,9 +1,9 @@
-module.exports.exportFunction = function(iddddd){
-  this.id = iddddd;
+module.exports.exportFunction = function(client){
+  this.ws = client;
   this.x = 0;
   this.y = 0;
   this.info = new Float64Array(3);
-    info[0] = iddddd;
+    info[0] = client.id;
     info[1] = 0;
     info[2] = 0;
   this.update = function(x,y){
