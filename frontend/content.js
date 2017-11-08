3759f4f385003fcded94ab7d27e3dd67fed09dd2
rsambouncer <rsambouncer@gmail.com>
2017-11-08 12:02:04 -0800

    Update content.js

--- frontend/content.js
+++ frontend/content.js
@@ -1,1 +1,10 @@
+(function() {
+  var canvas = document.getElementById('myCanvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
 
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
+  }
+}());
