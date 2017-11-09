eb2cbf10beee33a2968bc0f4c00c8913d1ded8a1
rsambouncer <rsambouncer@gmail.com>
2017-11-09 14:19:08 -0800

    Update game.js

--- backend/game.js
+++ backend/game.js
@@ -1,6 +1,7 @@
 module.exports.exportFunction = function(peoples){
   return function(){
     var infoBuffer = new ArrayBuffer(peoples.length*12);
+    console.log(peoples.length);
     var info = new Float32Array(infoBuffer);
     var a=0; for(var p in peoples){
       info[a++] = peoples[p].ws.id;
@@ -8,7 +9,12 @@ module.exports.exportFunction = function(peoples){
       info[a++] = peoples[p].y;
     }
     for(var p in peoples){
-      peoples[p].ws.send(infoBuffer);
+      var bbb = new ArrayBuffer(12);
+        var iii = new Float32Array(bbb);
+        iii[0] = 5;
+        iii[1] = 300;
+        iii[2] = 500;
+      peoples[p].ws.send(bbb);
     }
   };
 };
