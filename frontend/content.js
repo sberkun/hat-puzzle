38910b1cc80b6269a61e69fd14e3ac3c357c751c
rsambouncer <rsambouncer@gmail.com>
2017-11-09 17:13:21 -0800

    Update content.js

--- frontend/content.js
+++ frontend/content.js
@@ -61,7 +61,7 @@
   document.addEventListener('mousemove',function(mouseE){
     mycoords[0] = mouseE.clientX;
     mycoords[1] = mouseE.clientY;
-    score = "x: ";
+    document.getElementById("score").innerHTML = "x: "+mycoords[0]+" y:"+mycoords[1];
   });
   ws.onmessage = function(event){
     allcoords = new Float32Array(event.data);
