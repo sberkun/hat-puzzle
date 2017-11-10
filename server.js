103401ebdaba785c27c067a34ad8c00eb0b3ec46
rsambouncer <rsambouncer@gmail.com>
2017-11-09 21:36:48 -0800

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,16 @@ wss.on('connection', (ws) => {
   ws.on('close', ()=> delete peoples[ws.id]);
 });
 
-setInterval(testaaa, 100);
+setInterval(function(){
+    var pplleenn = Object.keys(peoples).length;
+    var infoBuffer = new ArrayBuffer(pplleenn*12);
+    var info = new Float32Array(infoBuffer);
+    var a=0; for(var p in peoples){
+      info[a++] = peoples[p].ws.id;
+      info[a++] = peoples[p].x;
+      info[a++] = peoples[p].y;
+    }
+    for(var p in peoples){
+      peoples[p].ws.send(infoBuffer);
+    }
+}, 100);
